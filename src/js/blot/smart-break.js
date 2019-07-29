3b1d340bee3e23b803ee7bcbe31896c0624bddd0
gunther358 <damian.debicki@gmail.com>
2019-07-29 15:05:27 +0200

    refactored smartbreak, fixed newline appending to editor

--- src/js/blot/smart-break.js
+++ src/js/blot/smart-break.js
@@ -4,17 +4,18 @@ const Embed = Quill.import('blots/embed');
 const Break = Quill.import('blots/break');
 
 export default class SmartBreak extends Break {
-    static insertInto(parent, ref) {
+    insertInto(parent, ref) {
         Embed.prototype.insertInto.call(this, parent, ref)
     }
 
-    static length() {
+    length() {
         return 1;
     }
 
-    static value() {
+    value() {
         return '\n';
     }
 }
 
 SmartBreak.blotName = 'smartbreak';
+SmartBreak.tagName = 'BR';
--- src/js/meditor.js
+++ src/js/meditor.js
@@ -10,23 +10,8 @@ import ShortText from './format/short-text-format';
 import MediumText from './format/medium-text-format';
 import LongText from './format/long-text-format';
 import InterimSpeech from './blot/interim-speech';
-
-// import SmartBreak from './blot/smart-break';
-//TODO: move to blot (smart-break doesn't work tho)
-
-let Parchment = Quill.import('parchment');
-let Break = Quill.import('blots/break');
-let Embed = Quill.import('blots/embed');
-Break.prototype.insertInto = function (parent, ref) {
-  Embed.prototype.insertInto.call(this, parent, ref)
-};
-Break.prototype.length = function () {
-  return 1;
-};
-Break.prototype.value = function () {
-  return '\n';
-};
-Quill.register(Break);
+import SmartBreak from './blot/smart-break';
+import newlineMatcher from './matcher/newline-matcher';
 
 Quill.register({
   'modules/content': Content,
@@ -39,7 +24,7 @@ Quill.register({
   'formats/medium-text': MediumText,
   'formats/long-text': LongText,
   'blots/interim-speech': InterimSpeech,
-  // 'blots/smart-break': SmartBreak,
+  'blots/smart-break': SmartBreak,
 }, true);
 
 window.meditor = function (el) {
@@ -63,40 +48,13 @@ window.meditor = function (el) {
       statusbar: true,
       clipboard: {
         matchVisual: false,
+        matchers: [
+          ['BR', newlineMatcher]
+        ]
       },
       keyboard: {
         bindings: {
-          handleEnter: {
-            key: 13,
-            handler: function (range, context) {
-              if (range.length > 0) {
-                this.quill.scroll.deleteAt(range.index, range.length);  // So we do not trigger text-change
-              }
-
-              let lineFormats = Object.keys(context.format).reduce(function (lineFormats, format) {
-                if (Parchment.query(format, Parchment.Scope.BLOCK) && !Array.isArray(context.format[format])) {
-                  lineFormats[format] = context.format[format];
-                }
-                return lineFormats;
-              }, {});
-              let previousChar = this.quill.getText(range.index - 1, 1);
-              // Earlier scroll.deleteAt might have messed up our selection,
-              // so insertText's built in selection preservation is not reliable
-              this.quill.insertText(range.index, '\n', lineFormats, Quill.sources.USER);
-              if (previousChar == '' || previousChar == '\n') {
-                this.quill.setSelection(range.index + 2, Quill.sources.SILENT);
-              } else {
-                this.quill.setSelection(range.index + 1, Quill.sources.SILENT);
-              }
-              this.quill.selection.scrollIntoView(this.quill.container);
-              Object.keys(context.format).forEach((name) => {
-                if (lineFormats[name] != null) return;
-                if (Array.isArray(context.format[name])) return;
-                if (name === 'link') return;
-                this.quill.format(name, context.format[name], Quill.sources.USER);
-              });
-            }
-          },
+          //https://github.com/quilljs/quill/issues/252#issuecomment-292565427
           linebreak: {
             key: 13,
             shiftKey: true,
@@ -104,24 +62,31 @@ window.meditor = function (el) {
               let currentLeaf = this.quill.getLeaf(range.index)[0];
               let nextLeaf = this.quill.getLeaf(range.index + 1)[0];
 
-              //TODO: change to smartbreak after moving to new blot
-              this.quill.insertEmbed(range.index, 'break', true, 'user');
+              this.quill.insertEmbed(range.index, 'smartbreak', true, 'user');
 
               // Insert a second break if:
               // At the end of the editor, OR next leaf has a different parent (<p>)
               if (nextLeaf === null || (currentLeaf.parent !== nextLeaf.parent)) {
-                //TODO: change to smartbreak after moving to new blot
-                this.quill.insertEmbed(range.index, 'break', true, 'user')
+                this.quill.insertEmbed(range.index, 'smartbreak', true, 'user');
               }
 
               // Now that we've inserted a line break, move the cursor forward
-              this.quill.setSelection(range.index + 1, Quill.sources.SILENT)
+              this.quill.setSelection(range.index + 1, Quill.sources.SILENT);
             }
           }
         }
       }
     }
   });
+
+  //https://github.com/quilljs/quill/issues/252#issuecomment-292565427
+  let length = instance.getLength();
+  let text = instance.getText(length - 2, 2);
+
+  // Remove extraneous new lines
+  if (text === '\n\n') {
+    instance.deleteText(instance.getLength() - 2, 2)
+  }
 
   $(el).get(0).meditor = instance;
   return instance;
